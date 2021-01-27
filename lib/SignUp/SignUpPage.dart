aa4ae6e806555f534c353bf735df5994b4c71b80
her_usernameher_username <58968686+dunghk123@users.noreply.github.com>
2021-01-27 15:56:20 +0700

    first

---
+++ lib/SignUp/SignUpPage.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'FrostedGlassBox.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double contWidth = size.width * 0.90;
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: Image.asset("assets/images/background_1.png").image,
+                fit: BoxFit.fill),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 220,
+                height: 220,
+                padding: EdgeInsets.all(1),
+                child: Image.asset('assets/images/LOGIN_LOGO.png'),
+              ),
+              Center(
+                child: FrostedGlassBox(
+                  width: contWidth,
+                  height: contWidth,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 10),
+                          child: Text(
+                            "SIGN UP",
+                            style: TextStyle(
+                                color: Color(0xff6380AB),
+                                fontSize: 34,
+                                fontWeight: FontWeight.normal),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Container(
+                          width: 300,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.black12),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Email address",
+                                hintStyle: TextStyle(
+                                    color: Colors.grey, fontSize: 20)),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          width: 300,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.black12),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(
+                                    color: Colors.grey, fontSize: 20)),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Container(
+                          width: 300,
+                          height: 50,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Color(0xff2A3D71)),
+                          child: Center(
+                            child: Text(
+                              "LOGIN",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          "Forget Password?",
+                          style: TextStyle(
+                              color: Colors.grey[600],
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20),
+                        ),
+                        /*Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: () => print('Login with Facebook'),
+                              child: Container(
+                                height: 40,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: (Colors.black26),
+                                        offset: Offset(0, 2),
+                                        blurRadius: 6.0)
+                                  ],
+                                  image: DecorationImage(
+                                    image:
+                                        Image.asset("assets/icons/facebook.png")
+                                            .image,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () => print('Login with Google'),
+                              child: Container(
+                                height: 40,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: (Colors.black26),
+                                        offset: Offset(0, 2),
+                                        blurRadius: 6.0)
+                                  ],
+                                  image: DecorationImage(
+                                    image: Image.asset("assets/icons/gmail.png")
+                                        .image,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () => print('Login with Twitter'),
+                              child: Container(
+                                height: 40,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: (Colors.black26),
+                                        offset: Offset(0, 2),
+                                        blurRadius: 6.0)
+                                  ],
+                                  image: DecorationImage(
+                                    image:
+                                        Image.asset("assets/icons/twitter.png")
+                                            .image,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )*/
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                  child: Text(
+                "Developed by R&D Department (DPIST)",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 21),
+              ))
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+import 'SignUp/SignUpPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+final Color textColor = Colors.white.withOpacity(0.4);
